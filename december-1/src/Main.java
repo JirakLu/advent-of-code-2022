19230ae6997557129773a6948766ea83f930264b
JirakLu <jirak.lukas@gmail.com>
2022-12-01 15:44:36 +0100

    feat: second part of december 1st

--- december-1/src/Main.java
+++ december-1/src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,7 +20,11 @@ public class Main {
             e.printStackTrace();
         }
 
-        System.out.println(input);
+
+        TreeSet<Integer> totals = new TreeSet<>();
+        totals.add(0);
+        totals.add(0);
+        totals.add(0);
 
         int total = 0;
         int maxTotal = 0;
@@ -29,6 +34,12 @@ public class Main {
                 if (total > maxTotal) {
                     maxTotal = total;
                 }
+                if (total > totals.first()) {
+                    if (totals.size() == 3) {
+                        totals.remove(totals.first());
+                    }
+                    totals.add(total);
+                }
                 total = 0;
             } else {
                 total += Integer.parseInt(s);
@@ -36,5 +47,7 @@ public class Main {
         }
 
         System.out.println(maxTotal);
+
+        System.out.println(totals.stream().reduce(0, Integer::sum));
     }
 }
